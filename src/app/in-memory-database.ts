f822b07184e4a1b0b1ec7e790049aa70ae096086
Fabio <buenocorp.mobile@gmail.com>
2019-03-11 20:44:54 -0300

    adding entry list component

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -2,6 +2,8 @@ import { InMemoryDbService } from "angular-in-memory-web-api";
 
 import { Category } from "./pages/categories/shared/category.model";
 
+import { Entry } from "./pages/entries/shared/entry.model";
+
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
         const categories: Category[] = [
@@ -11,6 +13,11 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 4, name: "Salário", description: "Recebimento de salário" }
         ];
 
-        return {categories};
+        const entries: Entry[] = [
+            { id: 1, name: "Gás de cozinha", description: "teste", type: "expensive", amount: "70,00", date: "01/01/2019", paid: true, categoryId: categories[0].id, category: categories[0] } as Entry
+
+        ];
+
+        return { categories, entries };
     }
 }
