fae822bfd5468bb383ea8e3be877310546bf069b
Fabio <buenocorp.mobile@gmail.com>
2019-03-12 20:28:23 -0300

    adding association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,3 +1,5 @@
+import { Category } from './../../categories/shared/category.model';
+import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
@@ -12,7 +14,7 @@ export class EntryService {
 
   private apiPath: string = "api/entries";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -30,19 +32,48 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    //para funcionar com in-memory-database, deve criar objeto de ligação(category)
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+      })
     );
+
+    //casos normais,o objeto já vem preenchido do serviço 
+    /*
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+    */
+
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(this.apiPath, entry).pipe(
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.put(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
+    )
+
+    //casos normais,o objeto já vem preenchido do serviço 
+    /*return this.http.put(this.apiPath, entry).pipe(
       catchError(this.handleError),
       map(() => entry)
-    );
+    );*/
   }
 
   delete(id: number): Observable<any> {
